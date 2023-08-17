3e93b50a447b3d8bb65d85e0e6818a1afcb74f5d
Juanpuppin <juan.puppin.monteiro@gmail.com>
2023-08-17 12:20:35 +0100

    v2

--- ServiceWorker.js
+++ ServiceWorker.js
@@ -1,33 +1,6 @@
-const cacheName = "Juan Puppin-Switch Up - Metamundo-0.1";
-const contentToCache = [
-    "Build/Switch Up Webgl.loader.js",
-    "Build/Switch Up Webgl.framework.js",
-    "Build/Switch Up Webgl.data",
-    "Build/Switch Up Webgl.wasm",
-    "TemplateData/style.css"
-
-];
 
 self.addEventListener('install', function (e) {
     console.log('[Service Worker] Install');
     
-    e.waitUntil((async function () {
-      const cache = await caches.open(cacheName);
-      console.log('[Service Worker] Caching all: app shell and content');
-      await cache.addAll(contentToCache);
-    })());
 });
 
-self.addEventListener('fetch', function (e) {
-    e.respondWith((async function () {
-      let response = await caches.match(e.request);
-      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
-      if (response) { return response; }
-
-      response = await fetch(e.request);
-      const cache = await caches.open(cacheName);
-      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
-      cache.put(e.request, response.clone());
-      return response;
-    })());
-});
